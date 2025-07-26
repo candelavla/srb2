4ae23c153cd963f3d98e8ac7b65628b0ce521008
candelavla <lavla@duck.com>
2025-07-26 13:29:27 -0500

    change appname to cherry

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -2478,7 +2478,7 @@ INT32 I_StartupSystem(void)
 	I_OutputMsg("Linked with SDL version: %d.%d.%d\n",
 	 SDLlinked.major, SDLlinked.minor, SDLlinked.patch);
 #if SDL_VERSION_ATLEAST(2,0,22)
-	SDL_SetHint(SDL_HINT_APP_NAME, "Sonic Robo Blast 2");
+	SDL_SetHint(SDL_HINT_APP_NAME, "SRB2 Cherry");
 #endif
 	if (SDL_Init(0) < 0)
 		I_Error("SRB2: SDL System Error: %s", SDL_GetError()); //Alam: Oh no....
