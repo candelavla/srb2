a6d94d95b3ecbc182351b319889c87c641ae464b
Gustaf Alhäll <gustaf@hanicef.me>
2025-10-05 12:16:01 +0200

    Revert "Optimize line checks on line interception"
    This reverts commit 6d35bf2c3e730deafa49b9a54b2e8641ccc8d6c2.

--- src/p_maputl.h
+++ src/p_maputl.h
@@ -41,17 +41,7 @@ typedef boolean (*traverser_t)(intercept_t *in);
 boolean P_PathTraverse(fixed_t px1, fixed_t py1, fixed_t px2, fixed_t py2,
 	INT32 pflags, traverser_t ptrav);
 
-//
-// P_PointOnLineSide
-// Returns 0 or 1
-//
-FUNCMATH FUNCINLINE static ATTRINLINE INT32 P_PointOnLineSide(fixed_t x, fixed_t y, line_t *line)
-{
-	// use cross product to determine side quickly
-	INT64 v = ((INT64)y - line->v1->y) * line->dx - ((INT64)x - line->v1->x) * line->dy;
-	return v > 0;
-}
-
+INT32 P_PointOnLineSide(fixed_t x, fixed_t y, line_t *line);
 FUNCMATH fixed_t P_AproxDistance(fixed_t dx, fixed_t dy);
 void P_ClosestPointOnLine(fixed_t x, fixed_t y, line_t *line, vertex_t *result);
 void P_ClosestPointOnLine3D(const vector3_t *p, const vector3_t *line, vector3_t *result);
