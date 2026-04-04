fe2524b2aa2f2bc6df5b1e146e650ce77ae5d690
candelavla <lavla@duck.com>
2026-04-04 15:00:06 -0500

    xd

--- src/info.c
+++ src/info.c
@@ -7078,9 +7078,9 @@ mobjinfo_t mobjinfo[NUMMOBJTYPES] =
 		S_SPRK1,        // deathstate
 		S_NULL,         // xdeathstate
 		sfx_itemup,     // deathsound
-		38*FRACUNIT,    // speed
-		16*FRACUNIT,    // radius
-		24*FRACUNIT,    // height
+		30*FRACUNIT,    // speed
+		18*FRACUNIT,    // radius
+		34*FRACUNIT,    // height
 		0,              // display offset
 		100,            // mass
 		0,              // damage
@@ -7105,9 +7105,9 @@ mobjinfo_t mobjinfo[NUMMOBJTYPES] =
 		S_SPRK1,        // deathstate
 		S_NULL,         // xdeathstate
 		sfx_itemup,     // deathsound
-		38*FRACUNIT,    // speed
-		16*FRACUNIT,    // radius
-		24*FRACUNIT,    // height
+		30*FRACUNIT,    // speed
+		18*FRACUNIT,    // radius
+		34*FRACUNIT,    // height
 		0,              // display offset
 		100,            // mass
 		0,              // damage
@@ -7133,7 +7133,7 @@ mobjinfo_t mobjinfo[NUMMOBJTYPES] =
 		S_NULL,         // xdeathstate
 		sfx_None,       // deathsound
 		0,              // speed
-		17*FRACUNIT,     // radius
+		18*FRACUNIT,     // radius
 		34*FRACUNIT,    // height
 		0,              // display offset
 		100,            // mass
